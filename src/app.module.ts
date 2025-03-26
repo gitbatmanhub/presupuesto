462022c19cdb89ed0135b44cc3bfc48222888e09
EdwinDev <ecro98.98@gmail.com>
2025-03-25 21:45:45 -0500

    update: Packages files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './modules/auth/auth.module';
+import { CuentaModule } from './modules/cuenta/cuenta.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'admin',
+      password: 'admin',
+      schema: 'presupuesto',
+      synchronize: true,
+      entities: ['dist/**/*.entity{.ts,.js}'],
+      database: 'presupuesto',
+    }),
+    AuthModule,
+    CuentaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
